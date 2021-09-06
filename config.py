404e557f328c11c259a7032bd63f7d935fba5c0a
slem <65255101+sabrinalem@users.noreply.github.com>
2021-09-06 11:33:03 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-SPOTIPY_CLIENT_ID='f440f13ed4ae49688beb5263c91c1d71'
-SPOTIPY_CLIENT_SECRET='62a15ca7375d412a86218b528143654c'
+SPOTIPY_CLIENT_ID= secrets.SPOTIFY_CLIENT_ID
+SPOTIPY_CLIENT_SECRET=secrets.SPOTIFY_CLIENT_SECRET
 SPOTIPY_REDIRECT_URI='http://127.0.0.1:5000/'
 SCOPE= 'user-read-currently-playing user-read-email playlist-modify-private\
                                                  user-read-private user-read-email user-top-read playlist-modify-public\
