bd8a3947e6eb6b3c04f6b305deb867b5bf9424c2
slem <65255101+sabrinalem@users.noreply.github.com>
2021-09-06 12:06:00 -0700

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-SPOTIPY_CLIENT_ID= secrets.CLIENT_ID
-SPOTIPY_CLIENT_SECRET=secrets.CLIENT_SECRET
-SPOTIPY_REDIRECT_URI='http://127.0.0.1:5000/'
-SCOPE= 'user-read-currently-playing user-read-email playlist-modify-private\
-                                                 user-read-private user-read-email user-top-read playlist-modify-public\
-                                                 user-library-modify user-read-private user-library-read'
