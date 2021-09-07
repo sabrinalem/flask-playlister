d7206f8266af4a1ed81d68fbb5def97e2fe48e08
slem <65255101+sabrinalem@users.noreply.github.com>
2021-09-06 23:10:02 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-SPOTIPY_CLIENT_ID=''
-SPOTIPY_CLIENT_SECRET=''
+SPOTIPY_CLIENT_ID='f440f13ed4ae49688beb5263c91c1d71'
+SPOTIPY_CLIENT_SECRET='62a15ca7375d412a86218b528143654c'
 SPOTIPY_REDIRECT_URI='https://flask-playlister.herokuapp.com/'
 SCOPE= 'user-read-currently-playing user-read-email playlist-modify-private\
                                                  user-read-private user-read-email user-top-read playlist-modify-public\
